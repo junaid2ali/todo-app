777dae3c9426e59c53d186b33490abe4bf8bd519
Junaid Ali <junaid.ali@jalimpb18us.tmsw.com>
2020-03-29 16:45:27 -0500

    pushing project

---
+++ src/components/jokesData.js
@@ -0,0 +1,29 @@
+const jokesData = [
+    {
+        id: 1,
+        punchLine: "It's hard to explain puns to kleptomaniacs because they always take things literally"
+    },
+    {
+        id: 2,
+        question: "What's the best thing about Switzerland?",
+        punchLine: "I don't know but the flag is a big plus!"
+    },
+    {
+        id: 3,
+        question: "What is the best sports city?",
+        punchLine: "The one that wins the most championships!"
+    },
+    {
+        id: 4,
+        question: "What is the best sports city?",
+        punchLine: "The one that wins the most championships!"
+    },
+    {
+        id: 5,
+        question: "What is the best sports city?",
+        punchLine: "The one that wins the most championships!"
+    }
+
+]
+
+export default jokesData
---
+++ src/components/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Exercise 30 minutes",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Exercise 50 minutes",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "Exercise 30 minutes",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Exercise 30 minutes",
+        completed: false
+    }
+]
+
+export default todosData
